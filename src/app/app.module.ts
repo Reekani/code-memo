1128ea8db7ff0352eb0bb9483ba6df48dfab0567
pawel3st <ppmalecki@gmail.com>
2018-08-09 19:20:05 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import {LoginComponent} from "./login-module/login/login.component";
+import {LoginModule} from "./login-module/login.module";
+
+
+const appRoutes: Routes = [
+  {path: '', component: LoginComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LoginModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
